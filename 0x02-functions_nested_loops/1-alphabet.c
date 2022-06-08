c088513b21542c04f4e63c97b9df3c0d16d50dbc
OhiareYazid <isahyazid@yahoo.com>
2022-06-08 03:56:20 +0100

    hot fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,12 +6,13 @@
 **/
 
 void print_alphabet(void)
-{int j;
-    for (j = 'a'; j <= 'z'; j++)
-    {
+{
+int j;
+for (j = 'a'; j <= 'z'; j++)
+{
 
-    _putchar(j);
+_putchar(j);
 
-    }
-    _putchar('\n');
+}
+_putchar('\n');
 }
