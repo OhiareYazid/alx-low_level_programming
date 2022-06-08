fde264e919308d67a31ff2bcce3e8f843b061d0e
OhiareYazid <35582610+OhiareYazid@users.noreply.github.com>
2022-06-08 13:06:55 +0100

    Create 6-print_numberz.c

---
+++ 0x001-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print base 10 number with only 2 putchar
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
