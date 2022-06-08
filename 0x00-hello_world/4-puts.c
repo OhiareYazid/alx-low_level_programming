b24ae5ba698d115d49514deb0a9b60a96a361dd6
OhiareYazid <35582610+OhiareYazid@users.noreply.github.com>
2022-06-08 12:50:53 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
-#include <stdio.h> 
-  
- /** 
-  * main - Entry point 
-  * Description: prints "\"Programming is like building a multilingual puzzle" 
-  * Return: 0 
-  */ 
-  
- int main(void) 
- { 
-         puts("\"Programming is like building a multilingual puzzle"); 
-         return (0); 
- }
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
