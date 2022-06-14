1b3b89c751cf63527d0c54f70b36cca09c3a7785
OhiareYazid <isahyazid@yahoo.com>
2022-06-14 08:33:07 -0700

    hot fix

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * reset to 98 -> a function to update the reference of a pointer
+ * reset_to_98 -> a function to update the reference of a pointer
  * @n: pointer to n to be updated
  *
  **/
