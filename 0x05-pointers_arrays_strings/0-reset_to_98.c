c0cb697ea3ef3c239411fabe23f7c92c6ab32ead
OhiareYazid <isahyazid@yahoo.com>
2022-06-14 08:17:44 -0700

    remove exec. and change reset file

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,17 +1,11 @@
-#include "main.h" 
+#include "main.h"
   
  /** 
  * reset_to_98 -> a function to update the reference of a pointer 
  * @n: pointer to n to be updated 
- */ 
- void reset_to_98(int *n) 
- { 
-         int *num;
-
-         int x = 98;
-
-         num = &x;
-
-         *n = *num;
-         
- }
+ *
+ **/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
