13ef318038a6bb495a64655c0015e2321c3708d2
OhiareYazid <isahyazid@yahoo.com>
2022-06-15 00:33:24 -0700

    hot fix

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,21 +1,22 @@
 #include "main.h"
+#include <string.h>
 /**
  * puts2 -> puts2 function
  * @str: str para8meter
  * Return: No return
- *
- **/
+**/
+
 void puts2(char *str)
+
 {
-	int count = 0;
-	for (; count >= 0; count++)
-	{
-		if (str[count] == '\0')
-		{
-			_putchar('\n');
-			break;
-		}
-		else if (count % 2 == 0)
-			_putchar(str[count]);
-	}
-}   
+	int len, i;
+
+	len = strlen(str);
+
+	for (i = 0; i < len; i += 2)
+		 
+		_putchar(str[i]);
+
+	_putchar('\n');
+
+}
