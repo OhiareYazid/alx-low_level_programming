9650092dbd3eb98e53547e0a36d4c1888d585408
OhiareYazid <35582610+OhiareYazid@users.noreply.github.com>
2022-06-15 09:55:09 +0100

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /** 
-* _strcpy - Copy a string
-* @dest: Destination Value
-* @src : Source value
-* Return: the pointer to dest
+ * _strcpy - Copy a string
+ * @dest: Destination Value
+ * @src : Source value
+ * Return: the pointer to dest
 */
 char *_strcpy(char *dest, char *src)
 {
@@ -12,8 +12,8 @@ char *_strcpy(char *dest, char *src)
  {
        dest[i] = src[i];
  }
- 
+
  dest[i++] = '\0';
- 
+
  return (dest);
 }
