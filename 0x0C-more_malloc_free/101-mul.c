9f950f3ec10ae04236eea29a97a42f017fe9a23c
OhiareYazid <35582610+OhiareYazid@users.noreply.github.com>
2022-07-02 22:32:44 +0100

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,18 +1,18 @@
 #include <stdlib.h> 
- #include <stdio.h> 
- #include "main.h" 
-  
- /** 
+#include <stdio.h> 
+#include "main.h" 
+
+/** 
   * is_digit - checks if a string contains a non-digit char 
   * @s: string to be evaluated 
   * 
   * Return: 0 if a non-digit is found, 1 otherwise 
-  */ 
-  
+*/ 
+
  int is_digit(char *s) 
  { 
          int i = 0; 
-  
+
          while (s[i]) 
          { 
                  if (s[i] < '0' || s[i] > '9') 
@@ -56,17 +56,17 @@
  { 
          char *s1, *s2; 
          int len1, len2, len, i, carry, digit1, digit2, *result, a = 0; 
-  
+
          s1 = argv[1], s2 = argv[2]; 
-  
+
          if (argc != 3 || !is_digit(s1) || !is_digit(s2)) 
                  errors(); 
          len1 = _strlen(s1); 
          len2 = _strlen(s2); 
          len = len1 + len2 + 1; 
-  
+
          result = malloc(sizeof(int) * len); 
-  
+
          if (!result) 
                  return (1); 
          for (i = 0; i <= len1 + len2; i++) 
